1a6c68b016aef470f15f068c08391bcb1fd7d9a3
Poran chowdury <poran.chowdury@selise.ch>
2022-03-28 15:11:00 +0600

    lock and synchronized

--- src/main/java/com/demo/thread/lock/LockExample.java
+++ src/main/java/com/demo/thread/lock/LockExample.java
@@ -7,6 +7,14 @@ public class LockExample {
     /* lock is similar to java synchronized block but is more simplified */
     public static void main(String[] args) {
         Lock lock = new ReentrantLock();
+        /*
+        1.Synchronized block must be contained within a single method
+          lock.lock() and lock.unlock() can be called from different methods
+        2. lock.lock() and lock.unlock() provide the same visibility and happens before
+           guarantees as entering and the exiting a synchronized block
+        3. Synchronized blocks are always reentrant.Lock could decide not to be.
+        4. Synchronized blocks do not guarantee fairness . Lock can.
+         */
         lock.lock();
         // do something
 
